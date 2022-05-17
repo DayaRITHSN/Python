1fef8653d8a6c314a49834386ec80061c456b74c
DayaRITHSN <105696892+DayaRITHSN@users.noreply.github.com>
2022-05-17 15:40:30 +0530

    Create list.py

---
+++ List/list.py
@@ -0,0 +1,14 @@
+empty_list = list() # this is an empty list, no item in the list
+print(len(empty_list)) # 0
+
+fruits = ['banana', 'orange', 'mango', 'lemon']                     # list of fruits
+vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']      # list of vegetables
+animal_products = ['milk', 'meat', 'butter', 'yoghurt']             # list of animal products
+
+# Print the lists and it length
+print('Fruits:', fruits)
+print('Number of fruits:', len(fruits))
+print('Vegetables:', vegetables)
+print('Number of vegetables:', len(vegetables))
+print('Animal products:',animal_products)
+print('Number of animal products:', len(animal_products))
