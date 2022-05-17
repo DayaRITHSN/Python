578a2b28cc1294589d98ce25dd439765f0b65fc8
DayaRITHSN <105696892+DayaRITHSN@users.noreply.github.com>
2022-05-17 15:35:00 +0530

    Update strings.py

--- Strings/strings.py
+++ Strings/strings.py
@@ -1,1 +1,21 @@
+# Single line comment
+letter = 'P'                # A string could be a single character or a bunch of texts
+print(letter)               # P
+print(len(letter))          # 1
+greeting = 'Hello, World!'  # String could be  a single or double quote,"Hello, World!"
+print(greeting)             # Hello, World!
+print(len(greeting))        # 13
+sentence = "I am trying to cover python basics in 2 days"
+print(sentence)
 
+# String Concatenation
+first_name = 'Dayananda'
+last_name = 'BS'
+space = ' '
+full_name = first_name  +  space + last_name
+print(full_name) # Dayananda BS
+# Checking length of a string using len() builtin function
+print(len(first_name))  # 9
+print(len(last_name))   # 2
+print(len(first_name) > len(last_name)) # True
+print(len(full_name)) # 12
